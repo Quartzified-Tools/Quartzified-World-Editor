093556b6f66f067eb053bbe600447cdd46152544
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-08-13 12:00:15 +0200

    Updated namespaces

--- Editor/EditObject.cs
+++ Editor/EditObject.cs
@@ -1,87 +1,90 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public static class EditObject
+namespace Quartzified.Editor.WorldEditor
 {
-    public static GameObject GetSpawnObject(ScriptableObjectTool tool, List<GameObject> spawnObjects)
+
+    public static class EditObject
     {
-        // Check List Length is greater than 0
-        if (spawnObjects.Count <= 0)
+        public static GameObject GetSpawnObject(ScriptableObjectTool tool, List<GameObject> spawnObjects)
         {
-            Debug.LogWarning("No Objects to spawn have been set!\nSpawning was Canceled.");
-            return null;
-        }
-
-        // Default first object in list
-        GameObject go = spawnObjects[0];
-
-        // Get Random Object if requested
-        if (tool.randomObj)
-        {
-            int rnd = Random.Range(0, spawnObjects.Count);
-            go = spawnObjects[rnd];
-        }
-
-        // Ensure a valid object has been selected
-        if (go == null)
-        {
-            Debug.LogWarning("Reference to an object is missing.\nSpawning was Canceled");
-            return null;
-        }
-
-        return PrefabUtility.InstantiatePrefab(go) as GameObject;
-    }
-
-    public static void SetObjectRotation(ScriptableObjectTool tool, GameObject spawnedObj)
-    {
-        // Apply Random Rotatiopn
-        if (tool.randomRotation)
-        {
-            // Get random rotation value
-            Vector3 rndRotation = new Vector3(Random.Range(0, tool.randomRotValues.x), Random.Range(0, tool.randomRotValues.y), Random.Range(0, tool.randomRotValues.z));
-            // Apply random rotaton value
-            spawnedObj.transform.rotation = Quaternion.Euler(rndRotation) * spawnedObj.transform.rotation;
-        }
-    }
-
-    public static void SetObjectScale(ScriptableObjectTool tool, GameObject spawnedObj)
-    {
-        // Set Random overall scale
-        if (tool.randomScale)
-        {
-            float scale = Random.Range(tool.scaleRange.x, tool.scaleRange.y);
-            spawnedObj.transform.localScale = new Vector3(scale, scale, scale);
-        }
-
-        // Set Random specific scale
-        if (tool.randomScale3D)
-        {
-            float scaleX = Random.Range(tool.scaleXRange.x, tool.scaleXRange.y);
-            float scaleY = Random.Range(tool.scaleYRange.x, tool.scaleYRange.y);
-            float scaleZ = Random.Range(tool.scaleZRange.x, tool.scaleZRange.y);
-            spawnedObj.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
-        }
-    }
-
-    public static void SetObjectPosition(ScriptableObjectTool tool, Vector3 pos, GameObject spawnedObj)
-    {
-        if(tool.placementArea == 1)
-        {
-            if (tool.posOffset)
+            // Check List Length is greater than 0
+            if (spawnObjects.Count <= 0)
             {
-                pos += tool.posOffsetValues;
+                Debug.LogWarning("No Objects to spawn have been set!\nSpawning was Canceled.");
+                return null;
             }
 
-            spawnedObj.transform.position = pos;
-        }
-        else
-        {
-            Vector3 placementArea = (Random.insideUnitSphere * (tool.placementArea - 1));
-            placementArea.y = 0;
+            // Default first object in list
+            GameObject go = spawnObjects[0];
 
-            spawnedObj.transform.position = pos + placementArea;
+            // Get Random Object if requested
+            if (tool.randomObj)
+            {
+                int rnd = Random.Range(0, spawnObjects.Count);
+                go = spawnObjects[rnd];
+            }
+
+            // Ensure a valid object has been selected
+            if (go == null)
+            {
+                Debug.LogWarning("Reference to an object is missing.\nSpawning was Canceled");
+                return null;
+            }
+
+            return PrefabUtility.InstantiatePrefab(go) as GameObject;
+        }
+
+        public static void SetObjectRotation(ScriptableObjectTool tool, GameObject spawnedObj)
+        {
+            // Apply Random Rotatiopn
+            if (tool.randomRotation)
+            {
+                // Get random rotation value
+                Vector3 rndRotation = new Vector3(Random.Range(0, tool.randomRotValues.x), Random.Range(0, tool.randomRotValues.y), Random.Range(0, tool.randomRotValues.z));
+                // Apply random rotaton value
+                spawnedObj.transform.rotation = Quaternion.Euler(rndRotation) * spawnedObj.transform.rotation;
+            }
+        }
+
+        public static void SetObjectScale(ScriptableObjectTool tool, GameObject spawnedObj)
+        {
+            // Set Random overall scale
+            if (tool.randomScale)
+            {
+                float scale = Random.Range(tool.scaleRange.x, tool.scaleRange.y);
+                spawnedObj.transform.localScale = new Vector3(scale, scale, scale);
+            }
+
+            // Set Random specific scale
+            if (tool.randomScale3D)
+            {
+                float scaleX = Random.Range(tool.scaleXRange.x, tool.scaleXRange.y);
+                float scaleY = Random.Range(tool.scaleYRange.x, tool.scaleYRange.y);
+                float scaleZ = Random.Range(tool.scaleZRange.x, tool.scaleZRange.y);
+                spawnedObj.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+            }
+        }
+
+        public static void SetObjectPosition(ScriptableObjectTool tool, Vector3 pos, GameObject spawnedObj)
+        {
+            if (tool.placementArea == 1)
+            {
+                if (tool.posOffset)
+                {
+                    pos += tool.posOffsetValues;
+                }
+
+                spawnedObj.transform.position = pos;
+            }
+            else
+            {
+                Vector3 placementArea = (Random.insideUnitSphere * (tool.placementArea - 1));
+                placementArea.y = 0;
+
+                spawnedObj.transform.position = pos + placementArea;
+            }
         }
     }
 }
--- Editor/ScriptableObjectTool.cs
+++ Editor/ScriptableObjectTool.cs
@@ -1,51 +1,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class ScriptableObjectTool : ScriptableObject
+namespace Quartzified.Editor.WorldEditor
 {
-    [Header("Brush Settings")]
-    public float placementArea = 1;
+    [System.Serializable]
+    public class ScriptableObjectTool : ScriptableObject
+    {
+        [Header("Brush Settings")]
+        public float placementArea = 1;
 
-    [Header("Object Settings")]
-    public bool randomObj = false;
+        [Header("Object Settings")]
+        public bool randomObj = false;
 
-    [Header("Layer Settings")]
-    public bool useLayer = false;
-    public int editLayerMask;
+        [Header("Layer Settings")]
+        public bool useLayer = false;
+        public int editLayerMask;
 
-    [Header("Material Settings")]
-    public bool useMaterials = false;
+        [Header("Material Settings")]
+        public bool useMaterials = false;
 
-    [Header("Scatter Settings")]
-    public bool scatter = false;
-    public float scatterDistance = 1;
+        [Header("Scatter Settings")]
+        public bool scatter = false;
+        public float scatterDistance = 1;
 
-    [Header("Position Options")]
-    public bool posOffset;
-    public Vector3 posOffsetValues = Vector3.zero;
+        [Header("Position Options")]
+        public bool posOffset;
+        public Vector3 posOffsetValues = Vector3.zero;
 
-    [Header("Rotation Options")]
-    public bool randomRotation;
-    public Vector3 randomRotValues = Vector3.zero;
+        [Header("Rotation Options")]
+        public bool randomRotation;
+        public Vector3 randomRotValues = Vector3.zero;
 
-    [Header("Scale Options")]
-    public bool randomScale;
-    public bool randomScale3D;
-    [Space]
-    public Vector2 scaleRange = Vector2.one;
-    [Space]
-    public Vector2 scaleXRange = Vector2.one;
-    public Vector2 scaleYRange = Vector2.one;
-    public Vector2 scaleZRange = Vector2.one;
+        [Header("Scale Options")]
+        public bool randomScale;
+        public bool randomScale3D;
+        [Space]
+        public Vector2 scaleRange = Vector2.one;
+        [Space]
+        public Vector2 scaleXRange = Vector2.one;
+        public Vector2 scaleYRange = Vector2.one;
+        public Vector2 scaleZRange = Vector2.one;
 
-    [Space(12)]
-    [Header("Foliage Density")]
-    public bool halfExtent;
-    public bool quarterExtent;
+        [Space(12)]
+        [Header("Foliage Density")]
+        public bool halfExtent;
+        public bool quarterExtent;
 
-    [HideInInspector]
-    public List<GameObject> objectsPlaced = new List<GameObject>();
-    [HideInInspector]
-    public GameObject justPlacedObject;
+        [HideInInspector]
+        public List<GameObject> objectsPlaced = new List<GameObject>();
+        [HideInInspector]
+        public GameObject justPlacedObject;
+    }
 }
